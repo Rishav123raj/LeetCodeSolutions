124e895a4615b8ad4da7a6996e5e358b37c8c22a
Rishav Raj Bhagat <rishavrajbhagat123@gmail.com>
2024-10-19 11:11:09 +0530

    Time: 99 ms (36.96%), Space: 52.8 MB (27.24%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.java
@@ -1,20 +1,13 @@
 class Solution {
-    public static String invertString(String s){
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '1') {
-                sb.append('0');
-            } else {
-                sb.append('1');
-            }
-        }
-        return sb.toString();
-    }
-
-    public static String reverseString(String s){
+    public static String reversedInvertedString(String s){
         StringBuilder sb = new StringBuilder();
         for(int i = s.length() - 1; i >= 0; i--){
-            sb.append(s.charAt(i));
+            if(s.charAt(i) == '1'){
+                sb.append('0');
+            }
+            else{
+                sb.append('1');
+            }
         }
         return sb.toString();
     }
@@ -22,7 +15,7 @@ class Solution {
         String dp[] = new String[n];
         dp[0] = "0";
         for(int i = 1; i < n; i++){
-            dp[i] = dp[i - 1] + "1" + reverseString(invertString(dp[i - 1]));
+            dp[i] = dp[i - 1] + "1" + reversedInvertedString(dp[i - 1]);
         }
         
         String ans = dp[n - 1];
