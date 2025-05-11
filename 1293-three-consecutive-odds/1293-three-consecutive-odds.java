5b9bd5a826d6d22219a5095f6f53aeacc44bb872
Rishav Raj Bhagat <rishavrajbhagat123@gmail.com>
2025-05-11 15:08:38 +0530

    Time: 0 ms (100%), Space: 42.5 MB (43.42%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+
+        for(int num : arr){
+            if((num & 1) == 1){
+                count++;
+                if(count == 3) return true;
+            }
+            else{
+                count = 0;
+            }
+        }
+
+        return false;
+    }
+}
